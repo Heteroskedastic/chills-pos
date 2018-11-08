8268ff5119afb2059f8020ecf92ca1b79204d5fd
pprolancer <pprolancer@gmail.com>
2018-11-08 03:21:56 -0800

    - removed kombo

--- chills_pos/chills_pos/settings/base.py
+++ chills_pos/chills_pos/settings/base.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'kombu.transport.django',
     'bootstrap3',
     'social.apps.django_app.default',
     'django_filters',
